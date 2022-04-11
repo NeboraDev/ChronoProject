9f1848a8ab9c3b8fe0fae68d9ebf564e795215f3
Nebora <72881699+NeboraDev@users.noreply.github.com>
2022-04-11 14:08:29 -0400

    Simple Hello World Created to Test.

--- src/Application.java
+++ src/Application.java
@@ -2,6 +2,7 @@ public class Application {
 
     public static void main(String[] args)
     {
-
+        GUI gui = new GUI(400, 400);
+        gui.MenuWidgets();
     }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,9 +1,40 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI extends JFrame {
 
-    public GUI()
+    private int resX;
+    private int resY;
+
+    public GUI(int resX, int resY)
     {
-        setTitle("Chrono");
+
+        this.resX = resX;
+        this.resY = resY;
+
+        Image icon = Toolkit.getDefaultToolkit().getImage("img/logo.ico");
+        setIconImage(icon);
+        setTitle("Chrono"); //Sets the title of the Application.
+        setSize(resX, resY); //Sets the size of the window application.
+        setVisible(true); //Sets the window as visible.
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //Allows the X button to close.
+        setLocationRelativeTo(null);
+    }
+
+    public void MenuWidgets()
+    {
+
+        String[] fileText = {"Save", "Save As", "Export", "Import", "Settings", "Quit"};
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+
+        for (String s : fileText) {
+            JMenuItem size = new JMenuItem(s);
+            menu.add(size);
+        }
+
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
     }
 }
